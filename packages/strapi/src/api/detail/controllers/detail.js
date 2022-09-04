7aed26215b8f18c238f9d4887c1646b84d3de3e5
zakharovda95 <zakharovda95@gmail.com>
2022-09-04 16:28:51 +0400

    new strapi

--- packages/strapi/src/api/detail/controllers/detail.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  detail controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::detail.detail');
--- packages/strapi/src/api/detail/routes/detail.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * detail router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::detail.detail');
