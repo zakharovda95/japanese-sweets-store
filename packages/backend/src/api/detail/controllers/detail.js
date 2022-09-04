ad886238b9cbde60bfb6db7d58ab7dfc15bbfa36
zakharovda95 <zakharovda95@gmail.com>
2022-09-04 16:30:02 +0400

    new strapi

---
+++ packages/backend/src/api/detail/controllers/detail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  detail controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::detail.detail');
